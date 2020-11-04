bd7a4818e20b279b61b8810512cea54e143a4b95
Ali Yousefpoor <Ali@Mahdis-MacBook-Pro.local>
2020-11-04 16:39:46 +0330

    handle notification for Button in Home

--- app/src/main/java/com/example/uidesign/home/HomeFragment.kt
+++ app/src/main/java/com/example/uidesign/home/HomeFragment.kt
@@ -17,7 +17,6 @@ import androidx.core.app.NotificationCompat
 import androidx.fragment.app.Fragment
 import com.example.uidesign.MainActivity
 import com.example.uidesign.R
-import com.google.firebase.messaging.FirebaseMessaging
 
 class HomeFragment : Fragment() {
     private lateinit var subscribe: Button
@@ -31,5 +30,46 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        subscribe = view.findViewById(R.id.notificationButton)
+
+        subscribe.setOnClickListener {
+            showNotification()
+        }
+    }
+
+    private fun showNotification() {
+        val notificationID = 234
+        val context = requireContext()
+        val soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val notificationManager =
+            context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            val channelID = "my_channel_01"
+            val name: CharSequence = "my_channel"
+            val description = "This is my channel"
+            val importance: Int = NotificationManager.IMPORTANCE_HIGH
+            val mChannel = NotificationChannel(channelID, name, importance)
+            mChannel.description = description
+            mChannel.enableLights(true)
+            mChannel.lightColor = Color.RED
+            mChannel.enableVibration(true)
+            mChannel.setShowBadge(false)
+            notificationManager.createNotificationChannel(mChannel)
+
+            val builer = NotificationCompat.Builder(context, channelID)
+                .setSmallIcon(R.drawable.notification)
+                .setContentTitle("Firebase Notification")
+                .setContentText("My first FCM Notification in my App")
+                .setSound(soundUri)
+
+            val resultIntent = Intent(context, MainActivity::class.java)
+            val stackBuilder = TaskStackBuilder.create(context)
+            stackBuilder.addParentStack(MainActivity::class.java)
+            stackBuilder.addNextIntent(resultIntent)
+            val pendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT)
+
+            builer.setContentIntent(pendingIntent)
+            notificationManager.notify(notificationID, builer.build())
+        }
     }
 }
